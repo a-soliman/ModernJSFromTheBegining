8ba94775bc3d208c75ee361fce389cc8048b25f4
a-soliman <ahmed.soliman@programmer.net>
2018-06-10 20:59:48 -0700

    math

---
+++ math/js/main.js
@@ -0,0 +1,15 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Math
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.floor(2.9);
+val = Math.sqrt(64);
+val = Math.pow(8, 2);
+val = Math.min(2, 23, 13, 44);
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
