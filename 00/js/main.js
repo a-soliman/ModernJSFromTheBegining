a35177b9d89996368c63d1805ab053abc5fd42b6
a-soliman <ahmed.soliman@programmer.net>
2018-06-10 20:25:43 -0700

    types

---
+++ 00/js/main.js
@@ -0,0 +1,25 @@
+// PRIMATIVE TYPES
+let name = 'Ahmed';
+let age = 30;
+let family = true;
+let car = null;
+let future;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof family);
+console.log(typeof car);
+console.log(typeof feature);
+
+// REFRENCE 
+let hobies = ['movies', 'Music'];
+let address = {
+    street: 'Main Str.',
+    house: '123',
+    state: 'CA'
+};
+let DOB = new Date();
+
+console.log(typeof hobies);
+console.log(typeof address);
+console.log(typeof DOB);
