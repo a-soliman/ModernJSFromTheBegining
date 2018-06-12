97ee4ac7588fd2934e21e03f111a076447ae8819
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-12 14:57:44 -0700

    orgnized the files

---
+++ 00/window/js/main.js
@@ -0,0 +1,41 @@
+// WINDOW METHODS / OBJECTS/ PROPERTIES
+
+// Alert
+//alert('hello');
+
+// prompt
+//const input = prompt("name: ");
+
+// confirm
+// if ( confirm("Are you sure? ") ) {
+//     console.log('ok.');
+// }
+
+// height / width
+
+let val;
+
+//outer height / width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// location object
+val = window.location;
+val = window.location.search;
+
+// redirect
+// window.location.href = 'http://google.com/';
+
+
+// History
+val = window.history;
+
+
+// navigetor
+val = window.navigator;
+
+console.log(val);
