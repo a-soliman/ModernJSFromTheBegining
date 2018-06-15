933e931a5e02e6aa9eb93e96ee97bebfdfd36896
a-soliman <ahmed.soliman@programmer.net>
2018-06-14 20:15:16 -0700

    xhr

---
+++ 07-ajax/00/js/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.btn').addEventListener('click', loadData);
+
+function loadData() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'data.txt', true);
+    xhr.onload = function() {
+        if ( this.status === 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhr.send();
+}
