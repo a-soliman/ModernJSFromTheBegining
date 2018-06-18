068c5e032043d695a4a5a651c648927409cd425f
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-18 06:56:41 -0700

    working with classes

--- 01-DOM/event-listener/js/main.js
+++ 01-DOM/event-listener/js/main.js
@@ -1,30 +1,14 @@
-const clearButton = document.querySelector('a.clear-tasks');
-const card = document.querySelector('.card');
-const heading = document.querySelector('h5');
+class Person{
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-clearButton.addEventListener('click', runEvent);
-clearButton.addEventListener('dblclick', runEvent);
-clearButton.addEventListener('mousedown', runEvent);
-clearButton.addEventListener('mouseenter', runEvent);
-clearButton.addEventListener('mouseleave', runEvent);
-clearButton.addEventListener('mouseover', runEvent);
-clearButton.addEventListener('mouseout', runEvent);
-
-document.addEventListener('mousemove', updatexy);
-
-
-function runEvent( e ) {
-    e.preventDefault();
-    console.log(e.clientX)
-    console.log(`Event type: ${e.type}`);
+    greet() {
+        console.log('Hello');
+    }
 }
 
-function updatexy(e) {
-    const ele = document.querySelector('h5');
-    const x = e.clientX;
-    const y = e.clientY;
+const ahmed = new Person('Ahmed', 'soliman');
 
-    ele.innerText = `X: ${x}, Y: ${y}`;
-    const body = document.querySelector('body');
-    body.style.background = `rgb(${y}, ${x}, 40)`;
-}
+console.log(ahmed)
