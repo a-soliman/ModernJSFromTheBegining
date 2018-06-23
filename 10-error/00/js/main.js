d4264c15d702d7de681b913a137df88680b3b4cb
a-soliman <ahmed.soliman@programmer.net>
2018-06-23 11:02:08 -0700

    producing Reference, Type, Syntac and URI Errors

--- 10-error/00/js/main.js
+++ 10-error/00/js/main.js
@@ -1,7 +1,21 @@
 try {
-    myFunction();
+    // Reference error
+    //myFunction();
+    
+    // TypeError
+    //null.myFunction();
+
+    // Syntax Error
+    //eval('Hello World');
+
+    // URI Error
+    //decodeURIComponent('%');
+
 } catch(e) {
-    console.log('Caching: ', e);
+    console.log('Caching Error, details below ');
+    console.log('name: ', e.name);
+    console.log('message: ', e.message);
+
 } finally {
     console.log('This runs no metter what happens');
 }
