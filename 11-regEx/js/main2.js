8521ac608efb54262bd5cba4578c17a229c092d5
a-soliman <ahmed.soliman@programmer.net>
2018-06-24 09:53:20 -0700

    grouping quantifiers nd characterSets

--- 11-regEx/js/main2.js
+++ 11-regEx/js/main2.js
@@ -13,10 +13,24 @@ re = /h*llo/i; // Matches any character 0 or more times;
 re = /gre?a?y/i; // => optional character;
 re = /gre?a?y\?/i; 
 
+// Brackets [] - character Sets
+re = /gr[ea]y/i; // Must be an a or an e 
+re = /[GF]ray/; // Must be a G or F 
+re = /[A-Z]ray/; // Matches any uppercase letter;
+re = /[a-z]ray/;
 
+// Braces {} = Quantifiers
+re = /Hel{2}o/i; // must occur exactly {n} amount of times.
+re = /hel{2,4}o/i; // must occur between 2 and 4 times
+re = /hel{2,}o/i; // must occur at leat (n) times
+
+// Parentheses () - Grouping
+re = /([0-9]x){3}/; // grouping the () part then applying the quantifiers on the gruop
+
+re = /^([0-9]x){3}$/; // grouping the () part then applying the quantifiers on the gruop, then restrcting the begging and end
 
 // string to match
-const str = 'Gray?';
+const str = '3x3x3x3x';
 
 // log result
 const result = re.exec(str);
