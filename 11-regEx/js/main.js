b673e6849ebf43a7788b44561d962c7193859d56
a-soliman <ahmed.soliman@programmer.net>
2018-06-24 09:08:47 -0700

    flags

--- 11-regEx/js/main.js
+++ 11-regEx/js/main.js
@@ -1,5 +1,15 @@
 let re;
 re = /hello/;
+re = /hello/i;
+
+
+/* 
+FALGS
+-----
+
+i => case insensitive
+g => Glocal search
+*/
 
 console.log(re);
 console.log(re.source);
@@ -11,7 +21,7 @@ console.log('exec() match: ', result1);
 console.log('exec() No-match: ', result2);
 
 // test() => returns true or false
-result1 = re.test('hello world!');
+result1 = re.test('Hello world!');
 result2 = re.test('hey world!'); 
 console.log('test() match: ', result1);
 console.log('test() No-match: ', result2);
