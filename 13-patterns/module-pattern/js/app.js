051b96effb32adcaa289a291fc820dff46a4e4ec
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-26 07:23:17 -0700

    standered module pattern

---
+++ 13-patterns/module-pattern/js/app.js
@@ -0,0 +1,24 @@
+// basic structure
+
+/* 
+    STANDERD MODULE PATTERN 
+    -----------------------
+*/
+
+// const UICtrl = (function(){
+//     const text = 'Hello World!';
+//     const changeText = function() {
+//         const element = document.querySelector('h1');
+//         element.textContent = text;
+//     };
+
+//     return {
+//         callChangeText : function() {
+//             changeText();
+//             console.log(text);
+//         }
+//     };
+// })();
+
+// UICtrl.callChangeText();
+
